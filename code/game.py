0c80e53f5af8366601c6260d8adb5238f944d0d1
Mirna Krpić <90164011+mirna-k@users.noreply.github.com>
2023-01-06 13:25:07 +0100

    Add files via upload

--- code/game.py
+++ code/game.py
@@ -1,6 +1,6 @@
-import pygame, sys, time
+import pygame, sys, time, random
 from settings import *
-from sprites import Background, Bird
+from sprites import Background, Bird, Obsticle
 
 class Game():
     def __init__(self):
@@ -8,11 +8,44 @@ class Game():
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption('Flappy Bird')
         self.clock = pygame.time.Clock()
-
-        # sprite groups 
+        self.active = True
+        
+        # sprite groups
         self.all_sprites = pygame.sprite.Group()
+        self.pipe_sprites = pygame.sprite.Group()
+
+        # sprites
         Background(self.all_sprites)
         self.bird = Bird(self.all_sprites)
+
+        # timer
+        self.obsticle_timer = pygame.USEREVENT + 1
+        pygame.time.set_timer(self.obsticle_timer, 1500)
+
+        # font
+        self.font = pygame.font.Font('graphics/font/BD_Cartoon_Shout.ttf', 30)
+        self.score = 0
+        self.passing_pipe = False
+
+        # restart menu
+        self.restart_surface = pygame.image.load('graphics/ui/restart.png').convert_alpha()
+        self.restart_rect = self.restart_surface.get_rect(center = (WIDTH/2, HEIGHT/2))
+
+
+    def display_score(self):
+        score_surface = self.font.render(str(self.score), True, 'black')
+        score_rect = score_surface.get_rect(midtop = (WIDTH/2, HEIGHT/30))
+        self.screen.blit(score_surface, score_rect)
+    
+
+    def collisions(self):
+        if pygame.sprite.spritecollide(self.bird, self.pipe_sprites, False, pygame.sprite.collide_mask)\
+            or self.bird.rect.bottom <= -100 or self.bird.rect.bottom >= HEIGHT + 100:
+            for sprite in self.pipe_sprites.sprites():
+                sprite.kill()
+            self.bird.kill()
+            self.active = False
+
 
     def run(self):
         last_time = time.time()
@@ -22,15 +55,41 @@ class Game():
             dt = time.time() - last_time
             last_time = time.time()
 
+            if len(self.pipe_sprites) > 0:
+                if self.bird.rect.right < self.pipe_sprites.sprites()[0].rect.right and self.passing_pipe == False:
+                    self.passing_pipe = True
+                    
+                if self.passing_pipe and self.bird.rect.left > self.pipe_sprites.sprites()[0].rect.right:
+                    self.score += 1
+                    self.passing_pipe = False
+
             # event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == self.obsticle_timer and self.active:
+                    pipe_height = random.randint(200, 400)
+                    Obsticle('down', pipe_height, [self.all_sprites, self.pipe_sprites])
+                    Obsticle('up', pipe_height, [self.all_sprites, self.pipe_sprites])
 
-            self.all_sprites.update(dt)
-            self.all_sprites.draw(self.screen)
+            if pygame.key.get_pressed()[pygame.K_SPACE]:
+                if self.active:
+                    self.bird.jump()
+                else:
+                    self.bird = Bird(self.all_sprites)
+                    self.active = True
+                    self.score = 0
+
+            if self.active:
+                self.all_sprites.update(dt)
+                self.all_sprites.draw(self.screen)
+                self.collisions()
+            else:
+                self.screen.blit(self.restart_surface, self.restart_rect)
+            self.display_score()
 
             # game logic
             pygame.display.update()
+        
             self.clock.tick(FRAMERATE)
--- code/sprites.py
+++ code/sprites.py
@@ -21,6 +21,7 @@ class Background(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.pos = pygame.math.Vector2(self.rect.topleft)
 
+
     def update(self, dt):
         self.pos.x -= 300 * dt
         self.rect.x = round(self.pos.x)
@@ -28,33 +29,90 @@ class Background(pygame.sprite.Sprite):
         if self.rect.centerx <= 0:
             self.pos.x = 0
 
+
 class Bird(pygame.sprite.Sprite):
     def __init__(self, *groups):
         super().__init__(*groups)
-
         # image
         self.import_frames()
-        self.frame_index = 0
+        self.frame_index = 1
         self.image = self.frames[self.frame_index]
 
         # rect
         self.rect = self.image.get_rect(center = (WIDTH/6, HEIGHT/2))
         self.pos = pygame.math.Vector2(self.rect.topleft)
 
+        # movement
+        self.gravity = 600
+        self.direction = 0
+
+        # mask
+        self.mask = pygame.mask.from_surface(self.image)
+
+
     def import_frames(self):
         self.frames = []
-
         for i in range(1, 4):
             img = pygame.image.load(f'graphics/bird/bird{i}.png').convert_alpha()
             self.frames.append(img)
 
+
+    def apply_gravity(self, dt):
+        self.direction += self.gravity * dt
+        self.pos.y += self.direction * dt
+        self.rect.y = round(self.pos.y)
+
+
+    def jump(self):
+        self.direction = -400
+
+
     def animate(self, dt):
         self.frame_index += 5 * dt
         if self.frame_index >= len(self.frames):
-            self.frame_index = 0
-
+            self.frame_index = 1
         self.image = self.frames[int(self.frame_index)]
 
+
+    def rotate(self):
+        rotated_bird = pygame.transform.rotozoom(self.image, -self.direction * 0.05, 1)
+        self.image = rotated_bird
+        self.mask = pygame.mask.from_surface(self.image)
+
+
     def update(self, dt):
+        self.apply_gravity(dt)
         self.animate(dt)
+        self.rotate()
+
+
+class Obsticle(pygame.sprite.Sprite):
+    def __init__(self, position: str, pipe_height: int, *groups):
+        super().__init__(*groups)
+        self.image = pygame.image.load('graphics/obstacles/pipe.png').convert_alpha()
+        self.rect = self.image.get_rect()
+
+        x = WIDTH + 100
+        pipe_gap = 350
+
+        if position == "down":
+            y = pipe_height + int(pipe_gap/2)
+            self.rect.topleft = (x, y)
+        if position == "up":
+            y = pipe_height
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect.bottomleft = (x, y)
+
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+
+        # mask
+        self.mask = pygame.mask.from_surface(self.image)
+
+
+    def update(self, dt):
+        self.pos.x -= 400 * dt
+        self.rect.x = round(self.pos.x)
+        if self.rect.right <= -150:
+            self.kill()
+
 
